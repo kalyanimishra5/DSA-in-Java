8936e947325ec758e510e6c1ea07498b8b8f1de0
Kalyani <201123398+kalyanimishra5@users.noreply.github.com>
2026-08-25 23:57:21 +0530

    double fillped triangle pattern

--- DoubleFlippedTriangle2.java
+++ DoubleFlippedTriangle2.java
@@ -1,17 +1,17 @@
 import java.util.Scanner;
 
-public class DoubleFlippedTriangle {
+public class DoubleFlippedTriangle2 {
      public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         
         int n=sc.nextInt();
-      
+     
         for(int i=1;i<=n;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("  ");
             }
-            for(int j=1;j<=n-i;j++){
-                System.out.print(i);
+            for(int j=1;j<=n+1-i;j++){
+                System.out.print(i+" ");
                 
                
             }
